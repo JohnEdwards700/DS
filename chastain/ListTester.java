8788636afdadbf04e9d5186bec53f4e61a055510
ejchastain <117233058+ejchastain@users.noreply.github.com>
2023-04-12 09:44:03 -0500

    Update ListTester.java

--- chastain/ListTester.java
+++ chastain/ListTester.java
@@ -2,21 +2,22 @@ import java.util.HashMap;
 
 class ListTester {
 
-	public static void main(String[] args){
-		DSArrayList<String> l = new DSArrayList<String>(); 
+        public static void main(String[] args){
+                DSArrayList<DSArrayList<String>> l = new DSArrayList<>();
 
-		l.add("Hello");
-		l.add("Hey");
+                l.add(new DSArrayList<String>());
+                l.add(new DSArrayList<String>());
+                l.get(1).add("Hello");
 
-		System.out.println("Here is the list: " + l);
-		System.out.println("The list has size " + l.length());
-		System.out.println("The last element of the list is " + l.get(l.length()-1));
+                System.out.println("Here is the list: " + l);
+                System.out.println("The list has size " + l.length());
+                System.out.println("The last element of the list is " + l.get(l.length()-1));
 
-		HashMap<String, String> h = new HashMap<>();
-		h.put("Jake","Hello Class");
-		h.put("Xavier","Grover Cleveland is my favorite president");
+                HashMap<String, String> h = new HashMap<String,String>();
+                h.put("Jake","Hello Class");
+                h.put("Xavier","Grover Cleveland is my favorite president");
 
-		System.out.println("Jake says: " + h.get("Jake"));
-		System.out.println("Xavier says: " + h.get("Xavier"));
-	}
+                System.out.println("Jake says: " + h.get("Jake"));
+                System.out.println("Xavier says: " + h.get("Xavier"));
+        }
 }
